8d93faa044e173b872cb875d2a07ed890419339c
ParthGupte <75539624+ParthGupte@users.noreply.github.com>
2022-01-02 21:58:49 +0530

    marubozu

--- stockfunctions.py
+++ stockfunctions.py
@@ -5,6 +5,7 @@ import simplerbank as sbi
 import csv
 import math
 import datetime
+import pandas as pd
 nse = Nse()
 
 def fallbuy(stock_code):
@@ -24,3 +25,40 @@ def yearlyreturn(profit_percent,time):
     t = time.total_seconds()
     k = datetime.timedelta(365).total_seconds()
     return (((1+(profit_percent/100))**(k/t))-1)*100
+
+def marubozu(stock_code,day):
+    data = get_history(symbol=stock_code,start=day,end=day)
+    # print(data)
+    open, close, high, low = data['Open'].values, data['Close'].values, data['High'].values, data['Low'].values
+    print(open)
+    if open==low and close==high:
+        return {'stock_code':stock_code,'marubozu':True,'bull':True} 
+    elif open==high and close==low:
+        return {'stock_code':stock_code,'marubozu':True,'bull':False}
+    else:
+        return {'stock_code':stock_code,'marubozu':False,'bull':None}
+
+   
+# print(yearlyreturn(15,datetime.timedelta(days=90)))
+M = 0
+with open('Equity.csv') as csvfile:
+    spamreader = csv.reader(csvfile, dialect='excel')
+    first = True
+    day = datetime.date(2021,12,30)
+    while day<=datetime.date(2021,12,30):
+        for row in spamreader:
+            if first:
+                first = False
+                continue
+            else:
+                stock_code = row[0]
+                maru = marubozu(stock_code,day)
+                print(maru)
+                if maru['marubozu']:
+                    M += 1
+        day+=datetime.timedelta(1)
+# day = datetime.date(2021,12,30)
+# print(marubozu('infy',day))
+# print(M)
+# data = get_history(symbol='infy',start=day,end=day)
+# # print(data)
