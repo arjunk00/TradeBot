df010cf642f553ca4946c1d1f1fffdd4e3e2919d
ParthGupte <75539624+ParthGupte@users.noreply.github.com>
2022-01-07 12:06:10 +0530

    algo mode

---
+++ algotrade.py
@@ -0,0 +1,54 @@
+import logging
+from kitedata_postgres import *
+from kiteconnect import KiteTicker
+from stockfunctions import stock_code_to_token, token_to_stock_code
+from kiteconnect_trade import *
+import statistics as st
+import csv
+import time
+
+logging.basicConfig(level=logging.DEBUG)
+api_secret = 'rkvip6z4jhn1fn5rifnrtbh707ukaf8x'
+api_key = "t44a8jbiydzpqq8b"
+access_token = "uMyIYRVXusyVmRLVuVy4RgTd2eqfg8Cy"
+kws = KiteTicker(api_key, access_token)
+
+
+last_100 = []
+
+def on_ticks(ws, ticks):
+    for tick in ticks:
+        if len(last_100)<=100:
+            last_100.append(tick['last_price'])
+        else:
+            del last_100[0]
+            last_100.append(tick['last_price'])
+
+
+
+
+def on_connect(ws, response):
+    token = stock_code_to_token('IDEA')
+    ws.subscribe([token])
+
+    ws.set_mode(ws.MODE_LTP,[token])
+
+
+def on_close(ws, code, reason):
+    ws.stop()
+
+
+kws.on_ticks = on_ticks
+kws.on_connect = on_connect
+kws.on_close = on_close
+
+kws.connect(threaded=True)
+
+while True:
+    if last_100<99:
+        continue
+    else:
+        mode = st.mode(last_100)
+        print(mode)
+
+
