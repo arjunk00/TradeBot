b03393b9c2a208c453fbc2aa7c4be93506fd281c
Arjun Kulkarni <arjunkulkarni23@protonmail.com>
2022-05-21 17:40:05 +0530

    add volume webscraper

--- tools/webscraper.py
+++ tools/webscraper.py
@@ -6,11 +6,12 @@ import csv
 
 prices = []
 while True:
-    url = "https://www.google.com/finance/quote/NIFTY_50:INDEXNSE"
+    url = "https://www.google.com/finance/quote/BTC-USD"
     r = requests.get(url)
     soup = BeautifulSoup(r.text, 'html.parser')
 
     fetch = soup.find('div', {'class': 'YMlKec fxKbKc'}).text
+    volume = soup.find('p', {'class': 'h5Ghwc-IOpRCf'}).text
 
     price_string = (fetch[0:])
     s1 = price_string.translate({ord(','): None})
@@ -18,6 +19,7 @@ while True:
     price = float(s1)
     prices.append(price)
     print(price)
+    print(volume)
     with open('test.csv', 'a+') as file:
         reader = csv.reader(file)
         next(reader, None)
