1a0f79b026bd038a60b2a161bdf58cd366e5d8aa
Arjun Kulkarni <arjunkulkarni23@protonmail.com>
2022-06-16 15:48:36 +0530

    try queryscraper for fw testing

--- tools/csv_to_db.py
+++ tools/csv_to_db.py
@@ -1,11 +1,17 @@
 import psycopg2
-import tools.stockfunctions as stockfunctions
+import os
+# import tools.stockfunctions as stockfunctions
+conn=psycopg2.connect(database="training_data", user="admin", password="xtremebutter", host="192.168.1.63", port="5432")
+cur=conn.cursor()
+# print(stockfunctions.stock_code_to_token('IDEA'))
+L = os.listdir("/home/fernblade/TradeBot/tools/FullDataCsv")
+for i in L:
+    st = i[0:-4].lower()
+    # query = 'CREATE TABLE {} AS (SELECT * FROM aartiind__eq__nse__nse__minute);'.format(st)
+    # cur.execute(query)
+    file=open(r'/home/fernblade/TradeBot/tools/FullDataCsv/'+i, 'r')
+    cur.copy_from(file, st, sep=',')
+    conn.commit()
+    # file.close()
 
-print(stockfunctions.stock_code_to_token('IDEA'))
-
-# conn=psycopg2.connect(database="marubozu_data", user="arjun", password="1234", host="127.0.0.1", port="5432")
-# cur=conn.cursor()
-# file=open(r'NSE_tokens.csv', 'r')
-# cur.copy_from(file, 'nse_tokens', sep=',')
-# conn.commit()
-# file.close()
+conn.close()
