83caf3910b77eb2211697412fe282e888503a35f
Arjun Kulkarni <arjunkulkarni23@protonmail.com>
2022-10-30 10:43:11 +0530

    try lin reg plot

--- yfinance/plot.py
+++ yfinance/plot.py
@@ -1,17 +1,31 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+from sklearn import metrics
+lr = LinearRegression()
 
 df = pd.read_csv("ADANIPORTS__EQ__NSE__NSE__5MINUTE_CONVERGED.csv")
 
-df1 = np.array(df["close"])
-df1 = np.delete(df1,-1)
+X = np.array(df["close"])
+X = np.delete(X, -1)
+X = X.reshape(-1, 1)
 
-df2 = np.array(df["close"])
-df2 = np.delete(df2,0)
+y = np.array(df["close"])
+y = np.delete(y, 0)
+y = y.reshape(-1, 1)
 
-print(len(df1))
-print(len(df2))
+x_train, x_test, y_train, y_test = train_test_split(X,y, test_size=0.33, random_state=42)
 
-plt.scatter(df1, df2)
+lr.fit(x_train, y_train)
+print(lr.score)
+prediction = lr.predict(x_test)
+print(lr.score(x_train, y_train))
+print(lr.score(x_test,y_test))
+print(metrics.mean_squared_error(y_test,prediction))
+print(metrics.r2_score(y_test,prediction))
+
+plt.scatter(x_test, prediction)
+plt.scatter(x_test, y_test)
 plt.show()
